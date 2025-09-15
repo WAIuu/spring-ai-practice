03c2f3c0d5a86f77078775c9b5bc9a988c372230
xuxu <2192437396@qq.com>
2025-09-15 13:17:44 +0800

    spring-ai:quick start

--- mcp-client-demo/src/main/java/com/practice/ai/mcpclient/ChatController.java
+++ mcp-client-demo/src/main/java/com/practice/ai/mcpclient/ChatController.java
@@ -1,29 +1,13 @@
 package com.practice.ai.mcpclient;
 
-import io.modelcontextprotocol.client.McpAsyncClient;
-import io.modelcontextprotocol.spec.McpSchema;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
-import org.springframework.ai.chat.client.advisor.SafeGuardAdvisor;
-import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.chat.prompt.PromptTemplate;
-import org.springframework.ai.template.st.StTemplateRenderer;
-import org.springframework.ai.tool.ToolCallbackProvider;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
-
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/ai/chat")
@@ -33,25 +17,6 @@ public class ChatController {
 
     private final ChatClient chatClient;
 
-    @Autowired
-    private List<McpAsyncClient> mcpAsyncClients;
-
-    @RequestMapping("/map")
-    public Mono<McpSchema.CallToolResult> test() {
-        var mcpClient = mcpAsyncClients.get(0);
-
-        return mcpClient.listTools()
-                .flatMap(tools -> {
-                    log.info("tools: {}", tools);
-
-                    return mcpClient.callTool(
-                            new McpSchema.CallToolRequest(
-                                    "maps_weather",
-                                    Map.of("city", "北京")
-                            )
-                    );
-                });
-    }
     @GetMapping("/query")
     public String query(@RequestParam String message, @RequestParam("userId") String userId) {
 
